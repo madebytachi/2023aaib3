6c84c226b4a18a971d07dbeb9f9415c1c6f340ed
madebytachi <145088456+madebytachi@users.noreply.github.com>
2024-11-20 11:52:51 +0800

    week11

---
+++ week11/week11-1.py
@@ -0,0 +1,9 @@
+#week11-1.py
+table = {}
+table[9977341] = 9
+table[4433997] = 4
+
+print(table[9977341]) 
+print(table[4433997])
+if () not in table:
+  print("table[0]不存在")
---
+++ week11/week11-4a.cpp
@@ -0,0 +1,13 @@
+// week11-4.cpp part 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	while( cin >> n){
+		if(n==0) break;
+		
+		cout << n << "\n";
+	}
+}
