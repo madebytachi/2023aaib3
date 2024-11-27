e0c9460bb83e5d76cb610b1c43c79dbc701777d4
madebytachi <145088456+madebytachi@users.noreply.github.com>
2024-11-27 11:59:35 +0800

    week12

---
+++ week12/week12-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int val;
+};
+class Cat{
+public:
+    string name;
+};
+int main()
+{
+    Cat cat1;
+    cat1.name = "cattie";
+    Node node;
+    node.val = 1;
+}
---
+++ week12/week12-2.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ans = new ListNode(99, new ListNode(90));
+        return ans;
+    }
+};
